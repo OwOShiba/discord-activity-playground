c07c143dd4de3900f9d1a5b9901170c2b2e26e0d
OwOShiba <noahyap3@gmail.com>
2024-04-28 12:48:28 -0400

    aaaaa

--- client/main.js
+++ client/main.js
@@ -1,9 +1,116 @@
-import './style.css'
-import rocketLogo from '/rocket.png'
+import { DiscordSDK } from "@discord/embedded-app-sdk";
+
+import ittoPic from '/itto.png';
+import "./style.css";
+
+// Will eventually store the authenticated user's access_token
+let auth;
+
+const discordSdk = new DiscordSDK(import.meta.env.VITE_DISCORD_CLIENT_ID);
+
+setupDiscordSdk().then(() => {
+  console.log("Discord SDK is authenticated");
+
+  appendVoiceChannelName();
+  appendGuildAvatar();
+  // We can now make API calls within the scopes we requested in setupDiscordSDK()
+  // Note: the access_token returned is a sensitive secret and should be treated as such
+});
+
+async function setupDiscordSdk() {
+  await discordSdk.ready();
+  console.log("Discord SDK is ready");
+
+  // Authorize with Discord Client
+  const { code } = await discordSdk.commands.authorize({
+    client_id: import.meta.env.VITE_DISCORD_CLIENT_ID,
+    response_type: "code",
+    state: "",
+    prompt: "none",
+    scope: [
+      "identify",
+      "guilds",
+    ],
+  });
+
+  // Retrieve an access_token from your activity's server
+  const response = await fetch("/api/token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      code,
+    }),
+  });
+  const { access_token } = await response.json();
+
+  // Authenticate with Discord client (using the access_token)
+  auth = await discordSdk.commands.authenticate({
+    access_token,
+  });
+
+  if (auth == null) {
+    throw new Error("Authenticate command failed");
+  }
+}
 
 document.querySelector('#app').innerHTML = `
   <div>
-    <img src="${rocketLogo}" class="logo" alt="Discord" />
-    <h1>Hello, World!</h1>
+    <p><img style="display: block; margin-left: auto; margin-right: auto;" src="https://cdn.discordapp.com/attachments/1134999216922574879/1138107743480320050/IMG_7935.jpg?ex=662fa66f&amp;is=662e54ef&amp;hm=df976313de0782abb2214e80723746ebe0f1b5417fbd5c9ee3ffe427b736652b&amp;" alt="" width="355" height="452" /></p>
+    <h1 style="text-align: center;"><strong>ARATAKI ITTO ACTIVITY</strong></h1>
   </div>
 `;
+
+async function appendVoiceChannelName() {
+  const app = document.querySelector('#app');
+
+  let activityChannelName = 'Unknown';
+
+  // Requesting the channel in GDMs (when the guild ID is null) requires
+  // the dm_channels.read scope which requires Discord approval.
+  if (discordSdk.channelId != null && discordSdk.guildId != null) {
+    // Over RPC collect info about the channel
+    const channel = await discordSdk.commands.getChannel({channel_id: discordSdk.channelId});
+    if (channel.name != null) {
+      activityChannelName = channel.name;
+    }
+  }
+
+  // Update the UI with the name of the current voice channel
+  const textTagString = `Activity Channel: "${activityChannelName}"`;
+  const textTag = document.createElement('p');
+  textTag.textContent = textTagString;
+  app.appendChild(textTag);
+}
+
+async function appendGuildAvatar() {
+  const app = document.querySelector('#app');
+
+  // 1. From the HTTP API fetch a list of all of the user's guilds
+  const guilds = await fetch(`https://discord.com/api/v10/users/@me/guilds`, {
+    headers: {
+      // NOTE: we're using the access_token provided by the "authenticate" command
+      Authorization: `Bearer ${auth.access_token}`,
+      'Content-Type': 'application/json',
+    },
+  }).then((response) => response.json());
+
+  // 2. Find the current guild's info, including it's "icon"
+  const currentGuild = guilds.find((g) => g.id === discordSdk.guildId);
+
+  // 3. Append to the UI an img tag with the related information
+  if (currentGuild != null) {
+    const guildImg = document.createElement('img');
+    guildImg.setAttribute(
+      'src',
+      // More info on image formatting here: https://discord.com/developers/docs/reference#image-formatting
+      `https://cdn.discordapp.com/icons/${currentGuild.id}/${currentGuild.icon}.webp?size=128`
+    );
+    guildImg.setAttribute('width', '128px');
+    guildImg.setAttribute('height', '128px');
+    guildImg.setAttribute('style', 'border-radius: 50%;');
+    app.appendChild(guildImg);
+  }
+}
+
